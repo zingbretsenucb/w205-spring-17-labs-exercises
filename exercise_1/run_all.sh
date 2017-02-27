f15a25df18efd464f0cc3d1f7656fb1aa34bef0f
Zach Ingbretsen <zingbretsen@ischool.berkeley.edu>
2017-02-27 06:53:40 +0000

    Adds some details to README

--- exercise_1/run_all.sh
+++ exercise_1/run_all.sh
@@ -2,16 +2,16 @@
 
 # This pulls the data from the web source and puts the data into HDFS
 cd loading_and_modeling
-load_data_lake.sh
+sh load_data_lake.sh
 cd -
 
 # This transforms the data into a cleaner form
 cd transforming
-run_transformation.sh
+sh run_transformation.sh
 cd -
 
 # This will run the final SQL queries to answer 
 # the questions and print my interpretations
 cd investigations
-run_investigations.sh
+sh run_investigations.sh
 cd -
