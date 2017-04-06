c1cee6afd30d07e6a5e003941aa4443656b6e363
Zach Ingbretsen <zingbretsen@ischool.berkeley.edu>
2017-04-06 23:44:07 +0000

    Adds setup files

--- exercise_2/setup.sh
+++ exercise_2/setup.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
+# Setup for Ex2
+
+# Install required Python packages
+pip2.7 install tweepy
+pip2.7 install psycopg2==2.6.2
+
+# If there's an existing project, back it up
 mv extweetwordcount extweetwordcount.bak
+
+# Create new project
 sparse quickstart extweetwordcount
+
+# Copy files from designated project
 rsync -avz tweetwordcount/* extweetwordcount/
 cd extweetwordcount
+
+# Remove unnecessary files
 rm topologies/wordcount.clj
 rm virtualenvs/wordcount.txt
 rm src/spouts/words.py
-source setup_credentials.sh
+
+# Move the user's credentials to the proper place
+source setup_config.sh
--- exercise_2/setup_config.sh
+++ exercise_2/setup_config.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Move Twitter and postgres credentials where required
 cp twitter_credentials.config credentials/
 cp postgres_credentials.config extweetwordcount/src/bolts/
 cp postgres_credentials.config extweetwordcount/src/spouts/
