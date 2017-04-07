080de2ddfd44ccbea700aa0d568c613467c0fe5b
Zach Ingbretsen <zingbretsen@ischool.berkeley.edu>
2017-04-07 01:53:58 +0000

    Slightly increase timeout
    The spout is giving a lot of empty queue exceptions

--- exercise_2/extweetwordcount/src/spouts/tweets.py
+++ exercise_2/extweetwordcount/src/spouts/tweets.py
@@ -79,7 +79,7 @@ class Tweets(Spout):
 
     def next_tuple(self):
         try:
-            tweet = self.queue().get(timeout = 0.1) 
+            tweet = self.queue().get(timeout = 0.2) 
             if tweet:
                 self.queue().task_done()
                 self.emit([tweet])
