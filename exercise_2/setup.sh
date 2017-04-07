a285370dc277be411f36fdcfb21df8788f914976
Zach Ingbretsen <zingbretsen@ischool.berkeley.edu>
2017-04-07 00:14:28 +0000

    Separate setup from proj creation

--- exercise_2/setup.sh
+++ exercise_2/setup.sh
@@ -6,23 +6,6 @@
 pip2.7 install tweepy
 pip2.7 install psycopg2==2.6.2
 
-# This was run when setting up project
-# but does not need to be done again
-# If there's an existing project, back it up
-# mv extweetwordcount extweetwordcount.bak
-
-# # Create new project
-# sparse quickstart extweetwordcount
-
-# # Copy files from designated project
-# rsync -avz tweetwordcount/* extweetwordcount/
-# cd extweetwordcount
-
-# # Remove unnecessary files
-# rm topologies/wordcount.clj
-# rm virtualenvs/wordcount.txt
-# rm src/spouts/words.py
-
 # Move the user's credentials to the proper place
 source setup_config.sh
 
