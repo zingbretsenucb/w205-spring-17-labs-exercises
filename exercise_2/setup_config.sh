e292a2004c57faa00a37603c6a3a94f002fa161c
Zach Ingbretsen <zingbretsen@ischool.berkeley.edu>
2017-04-07 01:58:54 +0000

    Corrects setup_config filename

--- exercise_2/setup_config.sh
+++ exercise_2/setup_config.sh
@@ -4,4 +4,4 @@
 cp twitter_credentials.config credentials/
 cp postgres_credentials.config FetchResults/
 cp postgres_credentials.config extweetwordcount/src/bolts/credentials.config
-cp postgres_credentials.config extweetwordcount/src/spouts/credentials.config
+cp twitter_credentials.config extweetwordcount/src/spouts/credentials.config
